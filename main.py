5571d6652ecb9eb7b8fa4e37e0499f42ac970819
AlexanderHaan <haan.alex@outlook.de>
2018-12-12 17:11:28 +0100

    Some code fixing, but no more stuff

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-# coding: utf8
+"""
+GUI for Main
+"""
 import tkinter as tk
 
 
-class Main:
+def mittelwert(array):
+    """Berechnet Mittelwert"""
+    mittel = 0.0
+    for num in array:
+        mittel += float(num)
+    mittel = mittel / len(array)
+    return mittel
+
+
+class Main:  # pylint: disable=too-few-public-methods
+    """Hauptseite wird erstellt"""
     def __init__(self):
         self.master = tk.Tk()
         self.master.title("Hauptmenü")
@@ -15,11 +27,13 @@ class Main:
         self.master.mainloop()
 
     def zur_urliste(self):
+        """Zu Urliste()"""
         self.master.destroy()
         urliste()
 
 
 class Urliste:
+    """Urliste wird erstellt"""
     def __init__(self):
         self.master = tk.Tk()
         self.master.title("Urliste")
@@ -27,44 +41,40 @@ class Urliste:
         self.berechnen_button = tk.Button(self.master, text="Berechnen",
                                           command=self.berechnen)
         self.label = tk.Label(self.master, text="")
-        self.zurueck_Button = tk.Button(self.master, text="Zum Hauptmenü",
+        self.zurueck_button = tk.Button(self.master, text="Zum Hauptmenü",
                                         command=self.zurueck)
 
         self.text_urliste.pack()
         self.berechnen_button.pack()
         self.label.pack()
-        self.zurueck_Button.pack()
+        self.zurueck_button.pack()
 
         self.master.mainloop()
 
     def berechnen(self):
+        """bei berechnen_button"""
         liste = self.text_urliste.get("1.0", "end-1c")
         liste = liste.replace(" ", "")
         urliste_array = liste.split(",")
         try:
-            _mittelwert = Urliste.mittelwert(urliste_array)
+            _mittelwert = mittelwert(urliste_array)
             self.label['text'] = str(_mittelwert)
         except ValueError:
             self.label['text'] = 'Falsche Eingabe'
-        pass
-
-    def mittelwert(array):
-        mittelwert = 0.0
-        for num in array:
-                mittelwert += float(num)
-        mittelwert = mittelwert / len(array)
-        return mittelwert
 
     def zurueck(self):
+        """Zum Main"""
         self.master.destroy()
         main()
 
 
 def main():
+    """Hier fängt das Programm an."""
     Main()
 
 
 def urliste():
+    """Hier wird die Seite für die Urliste erstellt"""
     Urliste()
 
 
